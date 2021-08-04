70fde5ded2f9d63e36281c26dd508d0ecd313cec
AKatryukhin <A.Katryukhin@yandex.ru>
2021-08-04 16:31:46 +0300

    feat: added routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,40 @@
-import React from 'react'; 
-import Header from '../Header/Header'; 
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
+import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 function App() {
   return (
-
+    <div className='body'>
+      <Header />
+      <Switch>
+        <Route path='/signin'>
+          <Login />
+        </Route>
+        <Route path='/signup'>
+          <Register />
+        </Route>
+        <Route path='/'>
+          <Main />
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
