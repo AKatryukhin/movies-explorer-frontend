5859815d76e76996bb45e64400f6b07ea78b44de
AKatryukhin <A.Katryukhin@yandex.ru>
2021-08-12 11:18:59 +0300

    fear: added route to MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './MoviesCardList.css';
 import { movies, savedMovies } from '../../utils/constants.js';
 import MoviesCard from '../MoviesCard/MoviesCard.js';
 
-function MoviesCardList({ isSavedMovies = false }) {
+function MoviesCardList() {
   return (
     <section className='movies-cardlist section'>
-      <div className='movies-cardlist__items'>
-        {!isSavedMovies
-          ? movies.map((movie) => <MoviesCard key={movie._id} movie={movie} />)
-          : savedMovies.map((movie) => (
-              <MoviesCard key={movie._id} movie={movie} />
-            ))}
-      </div>
-      {!isSavedMovies ? (
+      <Route path='/movies'>
+        <div className='movies-cardlist__items'>
+          {movies.map((movie) => (
+            <MoviesCard key={movie._id} movie={movie} />
+          ))}
+        </div>
         <button
-          className='movies-cardlist__more-button'
-          aria-label='Показать еще'
-        >
-          Ещё
-        </button>
-      ) : (
+            className='movies-cardlist__more-button'
+            aria-label='Показать еще'
+          >
+            Ещё
+          </button>
+      </Route>
+      <Route path='/saved-movies'>
+        <div className='movies-cardlist__items movies-cardlist__items_type_saved'>
+          {savedMovies.map((movie) => (
+            <MoviesCard key={movie._id} movie={movie} />
+          ))}
+        </div>
         <div className='movies-cardlist__empty'></div>
-      )}
+      </Route>
     </section>
   );
 }
