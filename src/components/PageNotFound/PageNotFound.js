6d45850b5875e3da10fca26fd5cc1608d8a3cc38
AKatryukhin <A.Katryukhin@yandex.ru>
2021-08-13 11:09:14 +0300

    style PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,17 +3,15 @@ import { Link } from 'react-router-dom';
 
 import './PageNotFound.css';
 
-function PageNotFound () {
+function PageNotFound() {
   return (
-    <div className="not-found">
-      <h3 className="not-found__title">
-       404
-      </h3>
-      <p className="not-found__text">
-      Страница не найдена
-      </p>
-      <Link className="not-found__button" to="/">Назад</Link>
+    <div className='not-found'>
+      <h3 className='not-found__title'>404</h3>
+      <p className='not-found__text'>Страница не найдена</p>
+      <Link className='not-found__button' to='/'>
+        Назад
+      </Link>
     </div>
-  )
+  );
 }
-export default PageNotFound; 
+export default PageNotFound;
