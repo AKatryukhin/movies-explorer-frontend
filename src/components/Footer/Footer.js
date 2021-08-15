1a147d20c3d2140d385a94608ee0ac762c2bb71e
AKatryukhin <A.Katryukhin@yandex.ru>
2021-08-15 11:30:44 +0300

    fix: edit Links to another resurse

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,13 +11,13 @@ function Footer() {
       <div className='footer__links-container'>
         <p className='footer__copyright'>&copy; {new Date().getFullYear()}</p>
         <ul className='footer__links'>
-          <Link to='/' className='footer__link'>
+          <Link to={{ pathname: 'https://practicum.yandex.ru/' }} target='_blank' rel='noopener noreferrer' className='footer__link'>
             Яндекс.Практикум
           </Link>
-          <Link to='/' className='footer__link'>
+          <Link to={{ pathname: 'https://github.com/' }}  target='_blank' rel='noopener noreferrer' className='footer__link'>
             Github
           </Link>
-          <Link to='/' className='footer__link'>
+          <Link to={{ pathname: 'https://ru-ru.facebook.com/' }} target='_blank' rel='noopener noreferrer' className='footer__link'>
             Facebook
           </Link>
         </ul>
